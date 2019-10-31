b373d37928dbc2dea1b00ff9f61b206933c575d3
ggsuccess <ggsuccess@naver.com>
2019-10-31 17:29:45 +0900

    sign

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handleEmail = e => {
+    setEmail(e.target.value);
+  }
+  const handlePassword = e => {
+    setPassword(e.target.value);
+  }
   return <div>
     <div>회원가입</div>
-    이메일<input type="text" onChange></input>
-    암&nbsp;호<input type="password" onChange></input>
+    이메일<input type="text" value={email} onChange={handleEmail}></input>
+    암&nbsp;호<input type="password" value={password} onChange={handlePassword}></input>
     <button onClick>회원 가입</button>
   </div>
 }
