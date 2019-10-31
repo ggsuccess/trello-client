7fabf179a15caa2949f7c07d8c20efed06450678
ggsuccess <ggsuccess@naver.com>
2019-10-31 15:25:32 +0900

    loginComp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 
 const Login = () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const onChangeEmail = e => {
+    setEmail(e.target.value);
+  }
+  const onChangePassword = e => {
+    setPassword(e.target.value);
+  }
   return <div>
       <div>Login</div>
-      이메일<input type="text" onChange></input>
-      암&nbsp;호<input type="password" onChange></input>
+      이메일<input type="text" value={email} onChange={onChangeEmail}></input>
+      암&nbsp;호<input type="password" value={password} onChange={onChangePassword}></input>
       <button onClick>로그인</button>
     </div>
 }
