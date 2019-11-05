0a6f83dfe48378afdb34c8049e6ce96e0b2ab653
ggsuccess <ggsuccess@naver.com>
2019-11-05 21:05:21 +0900

    axios

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Signup = () => {
   const [ email, setEmail ] = useState('');
@@ -14,12 +15,21 @@ const Signup = () => {
   const handleName = e => {
     setName(e.target.value);
   }
+  const submitSignup = () => {
+    if (email === '' || name === '' || password === '') {
+      alert('이메일,이름,비밀번호를 모두 입력해야합니다');
+    } else {
+      axios.post('http://localhost:3000/adduser', { email: email, name: name, password: password })
+      .then(() => console.log('가입성공'))
+      .catch(err => console.log('가입실패에러:'+err));
+    }
+  }
   return <div>
     <div>회원가입</div>
     이메일<input type="text" value={email} onChange={handleEmail}></input>
     이름<input type="text" value={name} onChange={handleName}></input>
     암&nbsp;호<input type="password" value={password} onChange={handlePassword}></input>
-    <button onClick>회원 가입</button>
+    <button onClick={submitSignup}>회원 가입</button>
   </div>
 }
 
