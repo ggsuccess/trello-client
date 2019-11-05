5ce13553ba29ec4df888e575515b9833ded1b7f6
ggsuccess <ggsuccess@naver.com>
2019-11-05 19:41:06 +0900

    ing

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const Signup = () => {
   const [ email, setEmail ] = useState('');
+  const [ name, setName ] = useState('');
   const [ password, setPassword ] = useState('');
 
   const handleEmail = e => {
@@ -10,9 +11,13 @@ const Signup = () => {
   const handlePassword = e => {
     setPassword(e.target.value);
   }
+  const handleName = e => {
+    setName(e.target.value);
+  }
   return <div>
     <div>회원가입</div>
     이메일<input type="text" value={email} onChange={handleEmail}></input>
+    이름<input type="text" value={name} onChange={handleName}></input>
     암&nbsp;호<input type="password" value={password} onChange={handlePassword}></input>
     <button onClick>회원 가입</button>
   </div>
